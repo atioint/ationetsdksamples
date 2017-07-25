1f5570f3d1a1cc3793503bcd3e7f5fafc9b65ad1
Sebastian Renzi <srenzi@atioinc.com>
2017-07-25 18:34:00 -0300

    # added Interface sample

--- AtionetAuthSample/Program.cs
+++ AtionetAuthSample/Program.cs
@@ -5,31 +5,50 @@
     using Ationet.Native.Model.JsonObjects.Auth;
     using Ationet.Sdk.Constants;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     class Program
     {
-        static async void Main(string[] args)
+        static void Main(string[] args)
+        {
+            Task.Run(() => MakeCall());
+
+            Console.ReadKey();
+        }
+
+        static async void MakeCall()
         {
             ////First of all, we create anAuthOperation object to interact with Ationet Authorization Engine
-            var ationetSdk = new Ationet.Sdk.Auth.AuthOperations("https://native-beta.ationet.com", 20000);
+            var ationetSdk = new Ationet.Sdk.Auth.AuthOperations("https://native-beta.ationet.com");
 
             var executeCompletion = false;
 
             ////Since the SendPreAuthorization() method receives a AuthTransactionRequest object, we create one.
             var preAuthRequest = new AuthTransactionRequest
             {
-                ApplicationType = Ationet.Native.Model.Constants.ApplicationType.FleetControlSystem, ////Required field (use suggested value)
-                ProcessingMode = Ationet.Native.Model.Constants.ProcessingMode.HostProcessingRequired, ////Required field (use suggested value)
-                MessageFormatVersion = Ationet.Native.Model.Constants.MessageVersion.AuthMessageVersion, ////Required field (use suggested value)
-                EntryMethod = Ationet.Native.Model.Constants.EntryMethod.Unknown, ////Required field (use suggested value)
-                AccountType = Ationet.Native.Model.Constants.AccountType.ProprietaryGlobalFleetCard, ////Required field (use suggested value)
+                ApplicationType = Ationet.Native.Model.Constants.ApplicationType.FleetControlSystem,
+                ////Required field (use suggested value)
+                ProcessingMode = Ationet.Native.Model.Constants.ProcessingMode.HostProcessingRequired,
+                ////Required field (use suggested value)
+                MessageFormatVersion = Ationet.Native.Model.Constants.MessageVersion.AuthMessageVersion,
+                ////Required field (use suggested value)
+                EntryMethod = Ationet.Native.Model.Constants.EntryMethod.Unknown,
+                ////Required field (use suggested value)
+                AccountType = Ationet.Native.Model.Constants.AccountType.ProprietaryGlobalFleetCard,
+                ////Required field (use suggested value)
                 TerminalIdentification = "CH905ZJPV7LK", ////Required field (this value is configured in the portal)
                 SystemModel = "SystemModel", ////Required field (Third party app name or description)
                 SystemVersion = "1.0", ////Required field (Third party app version)
                 PrimaryTrack = "B3CF6C7AC98FD590", ////Required field (track2 information for primary identification)
-                TransactionSequenceNumber = 43, //// A sequence number that the terminal needs to store and handle
-                LocalTransactionDate = int.Parse($"{DateTime.Now.Year}{DateTime.Now.Month.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Day.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}"), //// YYYYMMDD
-                LocalTransactionTime = int.Parse($"{DateTime.Now.Hour.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Minute.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Second.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}") //// HHMMSS
+                TransactionSequenceNumber = 51, //// A sequence number that the terminal needs to store and handle
+                LocalTransactionDate =
+                    int.Parse(
+                        $"{DateTime.Now.Year}{DateTime.Now.Month.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Day.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}"),
+                //// YYYYMMDD
+                LocalTransactionTime =
+                    int.Parse(
+                        $"{DateTime.Now.Hour.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Minute.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Second.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}")
+                //// HHMMSS
             };
 
             //// Sends the Pre Auth to the host
@@ -77,31 +96,45 @@
             Console.WriteLine("");
 
 
-            var action = Console.ReadKey(true);
-            
+            var action = Console.ReadKey();
+
             ////If Send Completion is selected, we send the completion to Ationet, if NOT, the transaction is cancelled
             if (action.Key == ConsoleKey.C)
             {
                 ////Since the SendConfirmation() method receives a AuthTransactionRequest object, we create one.
                 var completionRequest = new AuthTransactionRequest
                 {
-                    ApplicationType = Ationet.Native.Model.Constants.ApplicationType.FleetControlSystem, ////Required field (use suggested value)
-                    ProcessingMode = Ationet.Native.Model.Constants.ProcessingMode.HostProcessingRequired, ////Required field (use suggested value)
-                    MessageFormatVersion = Ationet.Native.Model.Constants.MessageVersion.AuthMessageVersion, ////Required field (use suggested value)
-                    EntryMethod = Ationet.Native.Model.Constants.EntryMethod.Unknown, ////Required field (use suggested value)
-                    AccountType = Ationet.Native.Model.Constants.AccountType.ProprietaryGlobalFleetCard, ////Required field (use suggested value)
+                    ApplicationType = Ationet.Native.Model.Constants.ApplicationType.FleetControlSystem,
+                    ////Required field (use suggested value)
+                    ProcessingMode = Ationet.Native.Model.Constants.ProcessingMode.HostProcessingRequired,
+                    ////Required field (use suggested value)
+                    MessageFormatVersion = Ationet.Native.Model.Constants.MessageVersion.AuthMessageVersion,
+                    ////Required field (use suggested value)
+                    EntryMethod = Ationet.Native.Model.Constants.EntryMethod.Unknown,
+                    ////Required field (use suggested value)
+                    AccountType = Ationet.Native.Model.Constants.AccountType.ProprietaryGlobalFleetCard,
+                    ////Required field (use suggested value)
                     TerminalIdentification = "CH905ZJPV7LK", ////Required field (this value is configured in the portal)
                     SystemModel = "SystemModel", ////Required field (Third party app name or description)
                     SystemVersion = "1.0", ////Required field (Third party app version)
-                    PrimaryTrack = "B3CF6C7AC98FD590", ////Required field (track2 information for primary identification)
-                    TransactionSequenceNumber = preAuthRequest.TransactionSequenceNumber, ////Required field (track2 information for primary identification)
+                    PrimaryTrack = "B3CF6C7AC98FD590",
+                    ////Required field (track2 information for primary identification)
+                    TransactionSequenceNumber = preAuthRequest.TransactionSequenceNumber,
+                    ////Required field (track2 information for primary identification)
 
-                    LocalTransactionDate = int.Parse($"{DateTime.Now.Year}{DateTime.Now.Month.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Day.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}"), //// YYYYMMDD
-                    LocalTransactionTime = int.Parse($"{DateTime.Now.Hour.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Minute.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Second.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}"), //// HHMMSS
-                                                                                                                                                                                                                                                                                                                    ////ProductAmount = 99, 
+                    LocalTransactionDate =
+                        int.Parse(
+                            $"{DateTime.Now.Year}{DateTime.Now.Month.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Day.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}"),
+                    //// YYYYMMDD
+                    LocalTransactionTime =
+                        int.Parse(
+                            $"{DateTime.Now.Hour.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Minute.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}{DateTime.Now.Second.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0')}"),
+                    //// HHMMSS
+                    ////ProductAmount = 99, 
                     ProductUnitPrice = decimal.Parse("1.23"),
                     ProductQuantity = decimal.Parse("9.82"),
-                    AuthorizationCode = preAuthResponse.Content.AuthorizationCode ////Required field (use the Authorization Code returned in the Pre Auth)
+                    AuthorizationCode = preAuthResponse.Content.AuthorizationCode
+                    ////Required field (use the Authorization Code returned in the Pre Auth)
                 };
 
                 //// Sends the Completion to the host
@@ -138,21 +171,27 @@
             {
                 var cancellationRequest = new AuthTransactionRequest
                 {
-                    ApplicationType = Ationet.Native.Model.Constants.ApplicationType.FleetControlSystem, ////Required field (use suggested value)
-                    ProcessingMode = Ationet.Native.Model.Constants.ProcessingMode.HostProcessingRequired, ////Required field (use suggested value)
-                    MessageFormatVersion = Ationet.Native.Model.Constants.MessageVersion.AuthMessageVersion, ////Required field (use suggested value)
-                    EntryMethod = Ationet.Native.Model.Constants.EntryMethod.Unknown, ////Required field (use suggested value)
-                    AccountType = Ationet.Native.Model.Constants.AccountType.ProprietaryGlobalFleetCard, ////Required field (use suggested value)
+                    ApplicationType = Ationet.Native.Model.Constants.ApplicationType.FleetControlSystem,
+                    ////Required field (use suggested value)
+                    ProcessingMode = Ationet.Native.Model.Constants.ProcessingMode.HostProcessingRequired,
+                    ////Required field (use suggested value)
+                    MessageFormatVersion = Ationet.Native.Model.Constants.MessageVersion.AuthMessageVersion,
+                    ////Required field (use suggested value)
+                    EntryMethod = Ationet.Native.Model.Constants.EntryMethod.Unknown,
+                    ////Required field (use suggested value)
+                    AccountType = Ationet.Native.Model.Constants.AccountType.ProprietaryGlobalFleetCard,
+                    ////Required field (use suggested value)
                     TerminalIdentification = "CH905ZJPV7LK", ////Required field (this value is configured in the portal)
                     SystemModel = "SystemModel", ////Required field (Third party app name or description)
                     SystemVersion = "1.0", ////Required field (Third party app version)
-                    TransactionSequenceNumber = preAuthRequest.TransactionSequenceNumber, ////Required field (track2 information for primary identification)
+                    TransactionSequenceNumber = preAuthRequest.TransactionSequenceNumber,
+                    ////Required field (track2 information for primary identification)
 
                     OriginalData = new Dictionary<string, string>
                     {
-                        { "TransactionCode", preAuthRequest.TransactionCode},
-                        { "LocalTransactionDate", preAuthRequest.LocalTransactionDate.ToString()},
-                        { "LocalTransactionTime", preAuthRequest.LocalTransactionTime.ToString()}
+                        {"TransactionCode", preAuthRequest.TransactionCode},
+                        {"LocalTransactionDate", preAuthRequest.LocalTransactionDate.ToString()},
+                        {"LocalTransactionTime", preAuthRequest.LocalTransactionTime.ToString()}
                     },
                     LocalTransactionDate = preAuthRequest.LocalTransactionDate, //// YYYYMMDD
                     LocalTransactionTime = preAuthRequest.LocalTransactionTime, //// HHMMSS
@@ -173,8 +212,6 @@
                     Console.WriteLine("Response Text: {0}", cancelationResponse.Content.ResponseText);
                 }
             }
-
-            Console.Read();
         }
     }
 }
---
+++ AtionetInterfaceSample/AtionetInterfaceSample/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AtionetInterfaceSample
+{
+    using System;
+    using System.Threading.Tasks;
+    using Ationet.Sdk.Constants;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => MakeCall());
+
+            Console.ReadKey();
+        }
+
+        static async void MakeCall()
+        {
+            var ationetClient = new Ationet.Sdk.Interface.InterfaceOperations("https://native-beta.ationet.com", "{yourusername}", "{yourpassword}");
+
+            var transactions = await ationetClient.GetTransactionsAsync("CH9", "SystemModel", "1.0", DateTime.Now.AddMonths(-2));
+
+            if (transactions.Status != SdkResponseStatus.Ok)
+            {
+                Console.WriteLine("Status: {0}", transactions.Status);
+                Console.WriteLine("Error Message: {0}", transactions.ErrorMessage);
+            }
+            else
+            {
+                foreach (var transaction in transactions.Content)
+                {
+                    Console.WriteLine("{0} - {1} - {2}", transaction.AuthorizationCode, transaction.TransactionAmountDispensed, transaction.DateTime);
+                }
+            }
+        }
+    }
+}
